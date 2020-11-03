c566bd1894004eb85908f42510cafd8015a96e68
Nobuyuki Fujioka <n.fujioka@abacusmaths.info>
2020-11-03 01:13:12 +0000

    adds example for api call with csrf token in headers

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ var { randomBytes } = require('crypto');
 router.get('/', function(req, res, next) {
   req.session.userId = '123fdsafdsa';
   req.session.csrfToken = randomBytes(100).toString('base64'); // convert random data to a string
-  res.render('index', { title: 'Express', csrfToken: req.session.csrfToken });
+  res.render('index', { title: 'Express', csrf_token: req.session.csrfToken });
 });
 
 router.post('/submit', function(req, res, next) {
@@ -33,5 +33,23 @@ router.post('/submit', function(req, res, next) {
                    <strong>Successful request!</strong>
                    </p>`);
 });
+
+router.post('/api', (req, res, next) => {
+  const csrfToken = req.header('csrfToken');
+  if (req.session.userId !== '123fdsafdsa') {
+    return res.json({ message: 'You are not authorised.' });
+  }
+  if (!csrfToken) {
+    return res.json({ message: 'CSRF Token not included.' });
+  }
+
+  if (csrfToken !== req.session.csrfToken) {
+    return res.json({ message: 'CSRF tokens do not match.' });
+  }
+
+  console.log('req.body > ', req.body);
+  res.json({ message: 'Success!' });
+});
+
 
 module.exports = router;
